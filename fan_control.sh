38da9895b81a08deda52a98637d90bdde466ba2b
ryotaw <31751212+ryotaw@users.noreply.github.com>
2021-11-14 23:43:29 +0900

    Add a feature of check mode.

--- fan_control.sh
+++ fan_control.sh
@@ -4,16 +4,20 @@
 # Usage:
 # fan_control.sh start -> fun starts
 # fan_control.sh stop  -> fun stops
+# fan_control.sh check -> check CPU's temerature and do start/stop
 #
 # To run this script you need a root permission.
 # So run with sudo.
 # Change the GPIO_PIN to the GPIO pin number which your fan is connected.
 # 14 is the pin number which official Raspberry Pi 4 Case Fan is connected.
+# TEMP_THRESHOLD is a temperature to decide stop or start in check mode.
+# TEMP_THRESHOLD=70000 means 70.000 degrees celsius.
 
 GPIO_PIN=14
 GPIO_DIR=/sys/class/gpio
 GPIO_TARGET_DIR=${GPIO_DIR}/gpio${GPIO_PIN}
 GPIO_VALUE=0
+TEMP_THRESHOLD=70000
 
 if [ $# != 1 ]; then
   echo "Bad argument"
@@ -21,10 +25,18 @@ if [ $# != 1 ]; then
   exit 1
 fi
 
-if [ $1 == 'start' ];then
+if [ $1 = 'start' ];then
   GPIO_VALUE=1
-elif [ $1 == 'stop' ];then
+elif [ $1 = 'stop' ];then
   GPIO_VALUE=0
+elif [ $1 = 'check' ];then
+  TEMPERATURE=`cat /sys/class/hwmon/hwmon0/temp1_input`
+  echo "CPU temperature is ${TEMPERATURE}"
+  if [ ${TEMPERATURE} -gt ${TEMP_THRESHOLD} ];then
+    GPIO_VALUE=1
+  else
+    GPIO_VALUE=0
+  fi
 else
   echo "Bad argument"
   echo "usage: fan_control start|stop"
@@ -42,4 +54,3 @@ echo ${GPIO_VALUE} > ${GPIO_TARGET_DIR}/value
 echo "${GPIO_PIN}" > ${GPIO_DIR}/unexport
 
 exit 0
-
